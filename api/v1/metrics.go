3c482f593ad7422d52244fa2ebd58bd67830b788
ramkrishna-peg <ugadim51@gmail.com>
2026-04-17 13:35:59 +0530

    Fix: metrics registration panic and double registration

--- api/v1/metrics.go
+++ api/v1/metrics.go
@@ -1,12 +1,16 @@
 package v1
 
 import (
+	"sync"
+
 	"github.com/gofiber/adaptor/v2"
 	"github.com/gofiber/fiber/v2"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/collectors"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+var registerOnce sync.Once
 
 var (
 	ActivePlayers = prometheus.NewGauge(prometheus.GaugeOpts{
@@ -33,15 +37,20 @@ var (
 )
 
 func RegisterMetrics() {
-	prometheus.MustRegister(
-		collectors.NewGoCollector(),
-		collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}),
-		ActivePlayers,
-		PlayingPlayers,
-		TracksLoaded,
-		TrackLoadDuration,
-		WebSocketConnections,
-	)
+	registerOnce.Do(func() {
+		// Go and Process collectors are often already registered in the default registry.
+		// We use Register instead of MustRegister to avoid panics if they already exist.
+		_ = prometheus.Register(collectors.NewGoCollector())
+		_ = prometheus.Register(collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}))
+
+		prometheus.MustRegister(
+			ActivePlayers,
+			PlayingPlayers,
+			TracksLoaded,
+			TrackLoadDuration,
+			WebSocketConnections,
+		)
+	})
 }
 
 func MetricsHandler() fiber.Handler {
